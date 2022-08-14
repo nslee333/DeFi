79395c29f5a37a7ba5565ac43c6bf5ebaf7ba9f5
Nathan <nslee333@gmail.com>
2022-08-13 17:40:34 -0700

    Commit

--- ERC-20/my-app/pages/index.js
+++ ERC-20/my-app/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { ethers, providers, getNetwork } from "ethers";
 import Web3Modal from "web3modal";
-import { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants.js" 
 
 
@@ -12,49 +12,44 @@ export  default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
 
-
-  
-
-
-    const connectWallet = async () => {
-      try {
-        await getProviderOrSigner();
-        setWalletConnected(true);
-        console.log(walletConnected);
-      } catch (error) {
-       console.error(error) 
-      }
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.error(error);
     }
+  }
   
-
-    useEffect(() => {
-      if(!walletConnected) {
-        web3ModalRef.current = new Web3Modal({
-          network: "goerli",
-          providerOptions: {},
-          disableInjectedProvider: false,
-        });
-         connectWallet();
-      }
-    }, [walletConnected])
-    
-
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
-    
-    const {chainId} = await web3Provider.getNetwork();
-    if(chainId !== 5) {
-      window.alert("Change the network to goerli");
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
       throw new Error("Change the network to Goerli");
     }
 
     if (needSigner) {
-      let signer = web3Provider.getSigner();
+      const signer = web3Provider.getSigner();
       return signer;
     }
     return web3Provider;
   }
+
+  useEffect(() => {
+    if (!walletConnected) {
+      connectWallet();
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      
+
+    }
+  }, [walletConnected]);
 
 
   
