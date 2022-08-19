2560226f8c9da27f5192d3c6f103c51e6f6769e3
Nathan <nslee333@gmail.com>
2022-08-19 14:42:59 -0700

    Commit

--- ERC-20v2/hardhat/test/ERC20.js
+++ ERC-20v2/hardhat/test/ERC20.js
@@ -4,10 +4,10 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
-const { utils, FixedNumber, BigNumber } = require("ethers");
+const { utils, FixedNumber, BigNumber, formatEther } = require("ethers");
 
 describe("ERC20", function () {
-  it("Should Mint sucessfully", async () => {
+  it("Should Mint successfully", async () => {
     const deploy1 = await ethers.getContractFactory("ERC20");
     const deployedContract = await deploy1.deploy("Nate Token", "NT");
     await deployedContract.deployed();
@@ -15,14 +15,13 @@ describe("ERC20", function () {
 
 
 
-      const value = 0.000000000000000001 * 1;
-      const decimals = 18;
+      const value = 0.000001 * 1;
+      // const decimals = 18;
+
+      const newValue = utils.parseUnits(value.toString());
 
 
-      const newValue = utils.parseUnits(value.toString(), decimals);
-
-
-    console.log(utils.parseEther(value.toString()));
+      console.log(utils.parseEther(value.toString()));
 
 
 
@@ -32,25 +31,26 @@ describe("ERC20", function () {
           value: utils.parseEther(newValue.toString()),
         } 
       );
+      txn.gasLimit(2100000);
       await txn.wait();
 
 
 
   })
 
-  it("log the values from the contract", async () => {
-    const deploy1 = await ethers.getContractFactory("ERC20");
-    const deployedContract = await deploy1.deploy("Nate Token", "NT");
-    await deployedContract.deployed();
-    console.log("Successfully deployed");
+  // it("log the values from the contract", async () => {
+  //   const deploy1 = await ethers.getContractFactory("ERC20");
+  //   const deployedContract = await deploy1.deploy("Nate Token", "NT");
+  //   await deployedContract.deployed();
+  //   console.log("Successfully deployed");
 
 
 
-    const tx = await deployedContract.totalSupply();
-    console.log(tx);
+  //   const tx = await deployedContract.totalSupply();
+  //   console.log(tx);
 
 
-  });
+  // });
 
 
 
