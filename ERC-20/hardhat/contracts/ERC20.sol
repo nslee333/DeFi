b65eae8f692ee49a850a45f2c8d4e186b47201be
Nathan <nslee333@gmail.com>
2022-08-28 10:52:14 -0700

    Deleted a testing directory, moved all my work on ERC20 and tests to ERC-20

--- ERC-20/hardhat/contracts/ERC20.sol
+++ ERC-20/hardhat/contracts/ERC20.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.9;
 
 
+import "hardhat/console.sol";
+
 contract ERC20 {
 
     string public tokenName;
@@ -33,8 +35,6 @@ contract ERC20 {
         return tokenName;
     }
 
-
-
     function symbol() public view returns (string memory) {
         return tokenSymbol;
     }
@@ -51,10 +51,9 @@ contract ERC20 {
         return balances[_owner];
     }
 
-
     function transfer(address _to, uint256 _amount) public  {
-        require(_to != address(0), "Cannot transfer to the zero address");
-        require(_amount > 0, "Cannot transfer zero tokens");
+        require(_to != address(0), "Cannot transfer to the zero address.");
+        require(_amount > 0, "Cannot transfer zero tokens.");
 
         balances[msg.sender] -= _amount;
         balances[_to] += _amount;
@@ -64,26 +63,24 @@ contract ERC20 {
 
     function allowance(address _owner, address _spender) public view returns (uint256) {
         require(_owner != address(0), "Please enter a valid owner account.");
-        require(_spender != address(0), "Please enter a valid spender address");
+        require(_spender != address(0), "Please enter a valid spender address.");
         return allowances[_owner][_spender];
     }
 
     function approve(address _owner, address _spender, uint256 _amount) public {
-        require(_owner == msg.sender, "Cannot approve without the owner's confirmation");
-        require(_spender != address(0), "Cannot give approval to a zero address");
+        require(_owner == msg.sender, "Cannot approve without the owner's confirmation.");
+        require(_spender != address(0), "Cannot give approval to a zero address.");
+
 
         allowances[_owner][_spender] = _amount;
 
         emit Approval(_owner, _spender, _amount);
     }
 
-
-
     function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
-        require(allowance(_from, msg.sender) > 0, "You do not an allowance approved");
-        require(_from != address(0), "Cannot transfer from the zero address.");
+        require(allowance(_from, msg.sender) > 0, "You do not have an allowance approved.");
         require(_to != address(0), "Cannot transfer to the zero address.");
-        require(_amount >= balanceOf(_from), "The amount exceeds the balance of the sender's address.");
+        require(_amount <= balanceOf(_from), "The amount exceeds the balance of the sender's address.");
 
 
         balances[_from] = balances[_from] -= _amount;
@@ -95,13 +92,11 @@ contract ERC20 {
 
     }
 
-
-
     function increaseAllowance(address _owner, address _spender, uint256 _newValue) public returns (bool success) {
-        require(_owner != address(0), "Cannot withdraw funds from address(0)");
-        require(_spender != address(0), "Spender cannot be address(0)");
-        require(_newValue > 0, "Cannot increase allowance to zero");
-        require(_owner == msg.sender, "Only the owner can increase the allowance");
+        require(_owner != address(0), "Cannot withdraw funds from address(0).");
+        require(_spender != address(0), "Spender cannot be address(0).");
+        require(_newValue > 0, "Cannot increase allowance to zero.");
+        require(_owner == msg.sender, "Only the owner can increase the allowance.");
         
         allowances[_owner][_spender] = 0;
         allowances[_owner][_spender] = _newValue;
@@ -110,11 +105,11 @@ contract ERC20 {
     }
 
     function decreaseAllowance(address _owner, address _spender, uint256 _newValue) public returns (bool success) {
-        require(_owner != address(0), "Cannot withdraw funds from address(0)");
-        require(_spender != address(0), "Spender cannot be address(0)");
-        require(_newValue > 0, "Cannot decrease allowance to zero");
-        require(_owner == msg.sender, "Only the owner can decrease the allowance");
-
+        require(_owner != address(0), "Cannot withdraw funds from address(0).");
+        require(_spender != address(0), "Spender cannot be address(0).");
+        require(_newValue > 0, "Cannot decrease allowance to zero.");
+        require(_owner == msg.sender, "Only the owner can decrease the allowance.");
+        
         allowances[_owner][_spender] = 0;
         allowances[_owner][_spender] = _newValue;
 
@@ -124,33 +119,31 @@ contract ERC20 {
 
     function mint(uint256 amount) public payable {
         require(tokenSupply > 0, "Not enough supply");
-        require(msg.value > tokenPrice, "Not enough ether sent");
-        uint256 value = tokenPrice * amount;
-        require(msg.value >= value, "Not enough Ether sent");
-        
-        uint256 mintAmount = tokenSupply - msg.value;
+        require(msg.value > tokenPrice, "Not enough ether sent.");
+        // require(msg.value > value, "Not enough Ether sent");
 
-        require(mintAmount < tokenSupply, "Not enough Token Supply");
+       
+        uint256 mintAmount = tokenSupply - amount;
+        
+
+
+        require(mintAmount < tokenSupply, "Not enough Token Supply.");
+
 
         uint256 newTokenSupply = tokenSupply - mintAmount;
         tokenSupply = newTokenSupply;
         balances[msg.sender] += mintAmount;
-
     }
 
-
-
-
     modifier onlyOwner() {
-        require(msg.sender == _contractOwner, "Not the owner");
+        require(msg.sender == _contractOwner, "Not the owner.");
         _;
     }
 
     function etherTransfer(address _to) public onlyOwner {
         (bool sent,) = _to.call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
+        require(sent, "Failed to send Ether.");
     } 
-
 
     event Transfer(address _from, address _to, uint256 _amount);
 
@@ -159,6 +152,5 @@ contract ERC20 {
     receive() external payable {}
 
     fallback() external payable {}
-
 
 }
---
+++ ERC-20/hardhat/contracts/Testing.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// I built this contract just to test the ERC20 etherTransfer function's second require statement.
+
+contract Testing {
+    
+}
