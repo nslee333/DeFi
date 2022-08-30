b505e5981f2bbbb0a8480f2d7cae5a68639b2ac3
Nathan <nslee333@gmail.com>
2022-08-29 20:29:09 -0700

    More work on the contract.

--- ERC-20v2/hardhat/contracts/ERC20.sol
+++ ERC-20v2/hardhat/contracts/ERC20.sol
@@ -42,23 +42,35 @@ contract ERC20 {
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
-        require(_owner != address(0), "Address 0");
+        require(_owner != address(0), "Address 0.");
         return balances[_owner];
     }
 
-    function transfer(address _to, uint256 amount) public {
-        // 
+    function transfer(address _to, uint256 _amount) public {
+        require(_amount > 0, "Tranfer amount too small.");
+        require(balances[_msgSender()] > _amount, "You don't have enough to transfer.");
+        require(_to != address(0), "Cannot transfer to address 0.");
 
-        // Assign msg.sender === owner.
+        address owner = _msgSender();
 
-        
-
-
+        _transfer(owner, _to, _amount);
     }
 
     // allowance(owner, spender)
 
-    // approve(spender, amount)
+    
+
+    function approve(address spender, uint256 amount) public {
+        require(spender != address(0), "Cannot give approval to address 0.");
+        require(amount > 0, "Allowance amount too small.");
+        
+        address owner = _msgSender();
+
+        _approve(owner, spender, amount);
+
+        emit Approval(owner, spender, amount);
+
+    }
 
     // TransferFrom(from, to, amount).
 
@@ -66,28 +78,48 @@ contract ERC20 {
 
     // decreaseAllowance(spender, subtractedValue)
 
-    // _transfer(from, to, amount)
+    function _transfer(address _from, address _to, uint256 _amount) private {
+        require(_to != address(0), "Cannot transfer to address 0.");
+
+        _beforeTokenTransfer(_from, _to, _amount);
+
+        balances[_from] -= _amount;
+        balances[_to] += _amount;
+
+        emit Transfer(_from, _to, _amount);
+
+        _afterTokenTransfer(_from, _to, _amount);
+
+    }
 
     // _mint(account, amount)
 
     // _burn(account, amount)
 
+    function _approve(address _owner, address _spender, uint256 _amount) private {
+        
+
+
+    }
     // _approve(owner, spender, amount)
 
     // _spendALlowance(owner, spender, amount)
 
-    // _beforeTokenTransfer(from, to, amount)
+  
+    function _beforeTokenTransfer(address _from, address _to, uint256 _amount) private {}
 
-    // _afterTokenTransfer(from, to, amount)
+    function _afterTokenTransfer(address _from, address _to, uint256 _amount) private {}
 
-    function _msgSender() private returns (address) {
-        // 
+    function _msgSender() private view returns (address) {
+        return msg.sender;
     }
 
     // EVENTS: 
 
     // Transfer(from, to, value)
 
-    // Approval(owner, spender, value)
+    event Transfer(address from, address to, uint256 value);
+
+    event Approval(address from, address to, uint256 value);
 
 }
