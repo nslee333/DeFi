3426fc3425dff798d1e7ff233b2166d45c8dd14e
Nathan <nslee333@gmail.com>
2022-09-02 16:20:00 -0700

    Adding a contract and renaming another.

---
+++ UniswapV1/hardhat/contracts/Exchange.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+
+contract Exchange {
+
+    // Add liquidity.
+
+    // removeLiquidity.
+
+    // Eth to ERC20 Exchange.
+
+    // ERC to ETH. 
+
+    // getBalance of contract.
+
+    
+    
+}
+
+
