d1591566c20e012e14f4d5b0faacae3a5ca89714
Nathan <nslee333@gmail.com>
2022-09-03 17:59:12 -0700

    Work on the exchange contract, and started one of the tokens contracts.

--- UniswapV1/hardhat/contracts/Exchange.sol
+++ UniswapV1/hardhat/contracts/Exchange.sol
@@ -1,20 +1,75 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract Exchange {
 
-    // Add liquidity.
+contract Exchange is ERC20 {
 
-    // removeLiquidity.
+    address tokenContract;
 
-    // Eth to ERC20 Exchange.
+    constructor(address _tokenContract) ERC20("LiquidityToken", "LP") {
+        tokenContract = _tokenContract;
+    }
 
-    // ERC to ETH. 
+    
+    
+    function addLiquidity(uint256 tokenAmount, uint256 deadline) public payable returns (uint256) {
+        // Approve. 
+        // transferFrom(tokenContract, address(this), tokenAmount);
 
-    // getBalance of contract.
+        // Revert transaction if deadline has passed.
 
+        
+
+
+    }
+
+
+    /*
+   addLiquidity function.
+   1. LP adds an equal amount of ERC-20 and ETH.
+   2. Mints LP Tokens in exchange realitive to the tokens provided. 
+   3. Deadline.
+
+
+   removeLiquidity function.
+   1. Burns LP tokens.
+   2. Gets ERC-20 and ETH tokens returned according to the LP tokens burned.
+   3. Deadline.
+
+   tokenToEthSwap function.
+   - Sell ERC-20 and get ETH returned.
+   - Fee removed.
+
+   tokenToEthSwapTransfer
+   - Same as above plus sending to a receipient address.
+   - Fee removed.
+
+   ethToTokenSwap function.
+    - Sell ETH and get ERC-20 tokens back.
+    - Fee removed.
+
+    ethToTokenSwapTransfer
+    - Same as above plus sending to a receipient address.
+    - Fee removed.
+
+
+    currentLiquiditty, returns ERC-20 and ETH token reserves for display.
+
+
+
+   
+
+
+
+
+
+
+
+    
+    */
     
     
 }
---
+++ UniswapV1/hardhat/contracts/Solari.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Solari is ERC20 {
+    
+    
+    constructor () ERC20("Solari", "SL") {
+        
+    }
+}
