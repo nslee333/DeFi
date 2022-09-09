c1f061f1d71aa1f106e038576cf143c9b8606c5c
Nathan <nslee333@gmail.com>
2022-09-08 17:40:08 -0700

    Exchange.sol more functions.

--- UniswapV1/hardhat/contracts/Exchange.sol
+++ UniswapV1/hardhat/contracts/Exchange.sol
@@ -10,9 +10,18 @@ contract Exchange is ERC20 {
     address tokenContract;
     address LPTokenContract;
 
+    mapping(address => uint256) private liquidityBalance;
+
     constructor(address _tokenContract) ERC20("LiquidityToken", "LP") {
         tokenContract = _tokenContract;
-        LPTokenContract = ERC20(LiquidityToken);
+    }
+
+    function getLiquidityBalance(address owner) public returns (uint256) {
+        return liquidityBalance[owner];
+    }
+
+    function getReserves() public returns (uint256) {
+        return ERC20(tokenContract).balanceOf(address(this));
     }
 
     function addLiquidity(uint256 tokenAmount, uint256 deadline) public payable returns (uint256) {
@@ -22,14 +31,50 @@ contract Exchange is ERC20 {
         ERC20(tokenContract).transferFrom(msg.sender, address(this), tokenAmount);
 
         uint256 ethAmount = msg.value;
-        uint256 ERC20TokenAmount = tokenAmount;
-        uint256 liquidityAmount = ethAmount * ERC20TokenAmount; // Just minting the Invariable's amount of LP tokens to the LP.
+        uint256 liquidityAmount = ethAmount; // Just minting the Invariable's amount of LP tokens to the LP.
+        liquidityBalance[msg.sender] = liquidityAmount;
 
         _mint(msg.sender, liquidityAmount);
     }
 
-    function removeLiquidity(uint256 lpTokenAmount, uint256 deadline) public returns (uint256, uin256) {
+    function removeLiquidity(uint256 lpTokenAmount, uint256 deadline) public returns (uint256, uint256) {
         require(deadline > block.timestamp, "Deadline has passed.");
+        require(lpTokenAmount > 0, "Please increase the amount of liquidity you wish to burn.");
+
+
+        uint256 ethAmount = lpTokenAmount;
+        uint256 tokenAmount = lpTokenAmount;
+        address recipient = msg.sender;
+
+        liquidityBalance[recipient] -= lpTokenAmount;
+
+        (bool sent,) = recipient.call{value: lpTokenAmount}("");
+        require(sent, "Failed to send ether.");
+
+        ERC20(tokenContract).transfer(recipient, lpTokenAmount);
+
+        return (ethAmount, tokenAmount); 
+    }
+
+    function tokenToEthSwap(uint256 tokens, uint256 deadline) public returns (uint256) {
+        require(deadline > block.timestamp, "Deadline has passed.");
+        require(tokens > 0, "Cannot swap zero tokens");
+
+        uint256 tokenReserves = getReserves();
+        uint256 ethReserves = address(this).balance;
+
+        uint256 invariant = tokenReserves * ethReserves;
+
+        uint256 ethSwapped = ((tokens/invariant) * 100) / 97;
+        address recipient = msg.sender;
+
+        (bool sent,) = recipient.call{value: ethSwapped}("");
+        require(sent, "Failed to send ether.");
+        return ethSwapped;
+    }
+
+    function tokenToEthSwap(uint256 deadline) public returns (uint256) {
+        require(deadline > block.timestamp, "Deadline has passed");
         
     }
 
