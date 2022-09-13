63a2dc6774332a282b588cf50f402c5d6f9edb00
Nathan <nslee333@gmail.com>
2022-09-12 17:10:39 -0700

    Factory exchange first draft.

--- UniswapV1/Exchange_Factory_V1/contracts/Factory.sol
+++ UniswapV1/Exchange_Factory_V1/contracts/Factory.sol
@@ -4,11 +4,32 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 contract Exchange {
-    
+
+    string public namePair;
+    // address public tokenContract;
+
+    constructor (string memory _namePair) {
+        namePair = _namePair;
+        // tokenContract = _tokenContract;
+    }
 }
 
 
 
 contract Factory {
-    
+
+    Exchange[] exchanges;
+    uint256 public numExchanges;
+
+    function create(string memory _namePair) public payable returns (uint256 dexID) {
+        Exchange newDex = new Exchange(_namePair);
+        uint256 dexID = numExchanges++;
+        exchanges.push(newDex);
+        return dexID;
+    }
+
+    function getAddress(uint256 dexID) public view returns (Exchange) {
+        return exchanges[dexID];
+    }
+
 }
--- UniswapV1/Exchange_Factory_V1/test/Factory.ts
+++ UniswapV1/Exchange_Factory_V1/test/Factory.ts
@@ -4,6 +4,21 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Factory", function () {
+    it("Should create a new exchange and return the id.", async () => {
+        const CFF = await ethers.getContractFactory("Factory");
+        const factory = await CFF.deploy();
+        await factory.deployed();
+    
+        const tx1 = await factory.create("SL + ETH");
+        const tx2 = await factory.create("2");
+        const tx3 = await factory.create("3");
+        const tx4 = await factory.create("4");
 
+        const confirmation1 = await factory.getAddress(0);
+        const confirmation2 = await factory.getAddress(1);
+        const confirmation3 = await factory.getAddress(2);
+        const confirmation4 = await factory.getAddress(3);
+        console.log(confirmation1, confirmation2, confirmation3, confirmation4);
+    });
 
 });
