eefbde70044068e2e6e59925f1531c7913eb351a
Nathan <nslee333@gmail.com>
2022-09-12 17:29:50 -0700

    Factory outline

--- UniswapV1/Exchange_Factory_V1/contracts/Factory.sol
+++ UniswapV1/Exchange_Factory_V1/contracts/Factory.sol
@@ -1,17 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-contract Exchange {
+contract Exchange is ERC20 {
 
     string public namePair;
-    // address public tokenContract;
+    address public tokenContract;
+    mapping(address => uint256) private liquidityBalance;
 
-    constructor (string memory _namePair) {
+    constructor (string memory _namePair, address _tokenContract) ERC20("LiquidityToken", "LP") {
         namePair = _namePair;
-        // tokenContract = _tokenContract;
+        tokenContract = _tokenContract;
     }
+
+    function getLiquidityBalance(address owner) public view returns (uint256) {
+        return liquidityBalance[owner];
+    }
+
+    function getReserves() public view returns (uint256) {
+        return ERC20(tokenContract).balanceOf(address(this));
+    }
+
+    function addLiquidity(uint256 tokenAmount, uint256 deadline) public payable returns (uint256) {}
+
+    function removeLiquidity(uint256 lpTokenAmount, uin256 deadline) public returns (uint256) {}
+
+    function tokenToEthSwap(uint256 tokenAmount, uint256 deadline) public returns (uint256) {}
+
+    function tokenToEthSwapTransfer(uint256 tokenAmount, address recipient, uint256 deadline) public payable returns (bool success) {} 
+
+    function ethToTokenSwap(uint256 deadline) public payable returns (uin256) {}
+
+    function ethToTokenSwapTransfer(address recipient, uint256 deadline) public payable returns (bool success) {}
+
+    function currentReserves() public view returns (uint256, uint256) {
+        uint256 ethReserves = address(this).balance;
+        uint256 tokenReserves = getReserves();
+        return (ethReserves, tokenReserves);
+    }
+
 }
 
 
