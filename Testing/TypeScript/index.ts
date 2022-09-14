795d2192111fca1642aa9c4953f55f3c3bdd2858
Nathan <nslee333@gmail.com>
2022-09-14 07:59:20 -0700

    X * Y = K

--- Testing/TypeScript/index.ts
+++ Testing/TypeScript/index.ts
@@ -6,16 +6,21 @@
 
 
 
+
+
+
+
+
+
+
+
+
 let ETH: number = 50000;
 let BAT: number = 50000;
-const invariant: number = ETH * BAT;
-
-// console.log(invariant, "Invariant");
-// console.log(invariant/ETH, "Expected BAT reserves");
-// console.log(invariant/BAT, "Expected ETH Reserves.");
+let invariant: number = ETH * BAT;
 
 
-function tokenToEth(tokens: number) {
+function eth(tokens: number) {
     BAT += tokens;
     const expEth: number = invariant / BAT;
     const value: number = ETH - expEth;
@@ -23,17 +28,46 @@ function tokenToEth(tokens: number) {
 }
 
 
-function ethToToken(ether: number) {
-    ETH += ether;
-    const expBAT: number = invariant / (ETH * 100);
-    // console.log(expBAT);
-    const value: number = BAT - (expBAT/97);
+console.log(eth(500), "eth");
+
+ETH = 50000;
+BAT = 50000;
+invariant = ETH * BAT;
+
+
+
+function eth2(tokens: number) {
+    BAT += tokens;
+    const expEth = ((invariant / BAT) * 100) / 97; 
+    console.log(expEth, "expEth");
+    const value = ETH - expEth;
     return value;
 }
-console.log(tokenToEth(500), "Token To Eth");
-console.log(ethToToken(500), "Eth to Token");
 
-// console.log(invariant, "Invariant");
-// console.log(invariant/ETH, "Expected BAT reserves");
-// console.log(invariant/BAT, "Expected ETH Reserves.");
+console.log(eth2(500), "eth2");
+
+
+ ETH = 50000;
+ BAT = 50000;
+ invariant = ETH * BAT;
+
+
+function token(ether: number) {
+    ETH += ether;
+    const expBAT: number = invariant / ETH;
+    let value: number = BAT - expBAT;
+    return value;
+}
+console.log(token(500), "Eth to Token");
+
+ETH = 50000;
+BAT = 50000;
+invariant = ETH * BAT;
+
+function token2(ether: number) {
+
+}
+
+
+console.log((500 * 100) / 97)
 
